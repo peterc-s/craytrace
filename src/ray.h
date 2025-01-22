77756e8dd3e98e9d5a1b670301979cf38d3fe935
peterc-s <pjcsmail@gmail.com>
2025-01-22 17:29:23 +0000

    Add ray.h

---
+++ src/ray.h
@@ -0,0 +1,35 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+typedef struct {
+    Point3 orig;
+    Vec3 dir;
+} Ray;
+
+inline Ray ray() {
+    return (Ray){};
+}
+
+inline Ray ray_with(const Point3* orig, const Vec3* dir) {
+    return (Ray) {
+        .orig = *orig,
+        .dir = *dir,
+    };
+}
+
+inline const Point3* ray_origin(const Ray* ray) {
+    return &ray->orig;
+}
+
+inline const Vec3* ray_direction(const Ray* ray) {
+    return &ray->dir;
+}
+
+inline Point3 at(const Ray* ray, double t) {
+    Vec3 mult_dir = vec3_mult(&ray->dir, t);
+    return vec3_add(&ray->orig, &mult_dir);
+}
+
+#endif
